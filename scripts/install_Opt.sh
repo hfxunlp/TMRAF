ac6ee23b5cb47625c25ee57765a3e894df14fdb1
ano <hfxunlp@foxmail.com>
2018-05-13 11:23:35 +0800

    update dnsmasq to 2.79

--- scripts/install_Opt.sh
+++ scripts/install_Opt.sh
@@ -14,10 +14,11 @@ echo "-------------COPY-FILES---------------"
 
 if [ -d "$ROOTDIR/trunk/user/" ] ; then
 
-	# cleaning before bump openvpn to 2.4.5, nfs-utils to 2.3.1, wpa_supplicant to 2.6
+	# cleaning before bump openvpn to 2.4.5, nfs-utils to 2.3.1, wpa_supplicant to 2.6, dnsmasq to 2.79
 	rm -fr "$DESTDIR/trunk/user/openvpn/openvpn-2.4.x/"
 	rm -fr "$DESTDIR/trunk/user/nfsd/nfs-utils-1.2.3/"
 	rm -fr "$DESTDIR/trunk/user/wpa_supplicant/"
+	rm -fr "$DESTDIR/trunk/user/dnsmasq/dnsmasq-2.7x/"
 
 	cp -fRv "$ROOTDIR/trunk/user/" "$DESTDIR/trunk/"
 fi
