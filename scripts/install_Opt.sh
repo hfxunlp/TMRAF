4e4fb9b0b0d7a1cb84d4f3a14bd063a45b4e1f7b
ano <hfxunlp@foxmail.com>
2018-10-23 10:36:28 +0200

    add update comment

--- scripts/install_Opt.sh
+++ scripts/install_Opt.sh
@@ -14,7 +14,7 @@ echo "-------------COPY-FILES---------------"
 
 if [ -d "$ROOTDIR/trunk/user/" ] ; then
 
-	# cleaning before bump openvpn to 2.4.5, nfs-utils to 2.3.1, wpa_supplicant to 2.6, dnsmasq to 2.79
+	# cleaning before bump openvpn to 2.4.5, nfs-utils to 2.3.1, wpa_supplicant to 2.6, dnsmasq to 2.80, libxml2 to 2.9.8, libcurl to 7.61.1.
 	rm -fr "$DESTDIR/trunk/user/openvpn/openvpn-2.4.x/"
 	rm -fr "$DESTDIR/trunk/user/nfsd/nfs-utils-1.2.3/"
 	rm -fr "$DESTDIR/trunk/user/wpa_supplicant/"
